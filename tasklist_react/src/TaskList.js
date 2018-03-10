13db4ecb93bd22c8a17c0fdcfdca24928b00b8ce
WWK563388548 <wc563388548@gmail.com>
2018-03-11 05:33:31 +0900

    Loading initial data from api

--- tasklist_react/src/TaskList.js
+++ tasklist_react/src/TaskList.js
@@ -1,6 +1,39 @@
 import React, {Component} from 'react';
+const APIURL = '/api/tasks';
 
 class TaskList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          tasks: []
+        }
+      }
+    
+      componentWillMount() {
+        this.loadTasks();
+      }
+    
+      // Can reuse this for request other things
+      loadTasks(){
+        fetch(APIURL)
+        .then(response => {
+            if(!response.ok) {
+                if(response.status >= 400 && response.status < 500){
+                    return response.json().then(data => {
+                        let err = {errorMessage: data.message};
+                        throw err;
+                    })
+                } else {
+                    let err = {errorMessage: "Please try again later. Server is not responding..."};
+                    throw err;
+                }
+            }
+            return response.json();
+    
+      }).then(tasks => this.setState({tasks}));
+    }
+
     render(){
         return (
             <h1>工作任务</h1>
