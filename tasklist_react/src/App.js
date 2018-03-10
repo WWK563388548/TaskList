88fb8753328304c29ec0724834cc5249c5c2ed51
WWK563388548 <wc563388548@gmail.com>
2018-03-11 04:59:03 +0900

    Initialized the front files of TaskList

---
+++ tasklist_react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TaskList from './TaskList';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TaskList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ tasklist_react/src/TaskList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TaskList extends Component {
+    render(){
+        return (
+            <h1>工作任务</h1>
+        )
+    }
+}
+
+export default TaskList; 
