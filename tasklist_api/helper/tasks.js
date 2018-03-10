173e5e36c353aeee69af8911623c40ea495bc6be
WWK563388548 <wc563388548@gmail.com>
2018-03-10 19:08:31 +0900

    refactored code

---
+++ tasklist_api/helper/tasks.js
@@ -0,0 +1,48 @@
+var db = require("../models");
+
+exports.getTasks = function(req, res){
+    db.Task.find().then(function(tasks){
+        res.json(tasks);
+    }).catch(function(err){
+        res.send(err);
+    });
+}
+
+exports.createTask = function(req, res){
+    db.Task.create(req.body).then(function(newTask){
+        // The status 201 means that new data has been created
+        res.status(201).json(newTask);
+    }).catch(function(err){
+        res.send(err);
+    });
+}
+
+exports.getTask = function(req, res){
+    db.Task.findById(req.params.taskId)
+    .then(function(foundTask){
+        res.json(foundTask);
+    }).catch(function(err){
+        res.send(err);
+    });
+ }
+
+ exports.updateTask = function(req, res){
+    // The "new: true" returns new updated value
+    db.Task.findOneAndUpdat({_id: req.params.taskId}, req.body, {new: true})
+    .then(function(task){
+        res.json(task);
+    }).catch(function(err){
+        res.send(err);
+    });
+}
+
+exports.deleteTask = function(req, res){
+    db.Task.remove({_id: req.params.taskId})
+    .then(function(){
+        res.json({message: "删除任务!"});
+    }).catch(function(err){
+        res.send(err);
+    });
+}
+
+module.exports = exports;
--- tasklist_api/routes/tasks.js
+++ tasklist_api/routes/tasks.js
@@ -1,55 +1,16 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
+var helpers = require("../helper/tasks");
 
-// The get route
-router.get("/", function(req, res){
-    db.Task.find().then(function(tasks){
-        res.json(tasks);
-    }).catch(function(err){
-        res.send(err);
-    });
-});
+// The get and post route
+router.route('/')
+.get(helpers.getTasks)
+.post(helpers.createTask);
 
-// The post route
-router.post("/", function(req, res){
-    db.Task.create(req.body).then(function(newTask){
-        // The status 201 means that new data has been created
-        res.status(201).json(newTask);
-    }).catch(function(err){
-        res.send(err);
-    });
-});
-
-// show route
-router.get("/:taskId", function(req, res){
-   db.Task.findById(req.params.taskId)
-   .then(function(foundTask){
-       res.json(foundTask);
-   }).catch(function(err){
-       res.send(err);
-   });
-});
-
-// Update route
-router.put("/:taskId", function(req, res){
-    // The "new: true" returns new updated value
-    db.Task.findOneAndUpdat({_id: req.params.taskId}, req.body, {new: true})
-    .then(function(task){
-        res.json(task);
-    }).catch(function(err){
-        res.send(err);
-    });
-});
-
-// delete route
-router.delete("/:taskId", function(req, res){
-    db.Task.remove({_id: req.params.taskId})
-    .then(function(){
-        res.json({message: "删除任务!"});
-    }).catch(function(err){
-        res.send(err);
-    });
-});
+router.route("/:taskId")
+.get(helpers.getTask)
+.put(helpers.updateTask)
+.delete(helpers.deleteTask);
 
 module.exports = router;
